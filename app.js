f55596b88be8142fdc01747987718b2b910438b4
gautham-anantharaj <gautham.anantharaj@freshworks.com>
2020-05-04 10:23:52 +0530

    Added nav bar and logos

--- app.js
+++ app.js
@@ -3,15 +3,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var expressHBS = require('express-handlebars');
 var indexRouter = require('./routes/index');
 
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+// app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', expressHBS({ defaultLayout: 'layout', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,12 +24,12 @@ app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
